3f9810d8d2e804bb3509994cee021870a5f1f35b
kslysz <kslysz@gmail.com>
2020-04-17 15:50:34 +0200

    https://github.com/databucket/databucket-app/issues/14 - fixed

--- src/main/java/pl/databucket/web/filters/FiltersController.java
+++ src/main/java/pl/databucket/web/filters/FiltersController.java
@@ -4,6 +4,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -19,11 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import pl.databucket.exception.EmptyInputValueException;
-import pl.databucket.exception.ExceededMaximumNumberOfCharactersException;
-import pl.databucket.exception.FilterAlreadyExistsException;
-import pl.databucket.exception.IncorrectValueException;
-import pl.databucket.exception.ItemDoNotExistsException;
+import pl.databucket.exception.*;
 import pl.databucket.database.C;
 import pl.databucket.database.COL;
 import pl.databucket.database.Condition;
@@ -37,146 +34,148 @@ import pl.databucket.service.ResponseStatus;
 @RestController
 public class FiltersController {
 
-  private static final Logger logger = LoggerFactory.getLogger(FiltersController.class);
+    private static final Logger logger = LoggerFactory.getLogger(FiltersController.class);
 
-  private final DatabucketService databucketService;
+    private final DatabucketService databucketService;
 
-  public FiltersController(DatabucketService databucketService) {
-    this.databucketService = databucketService;
-  }
-
-  @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<ResponseBody> createFilter(@RequestParam String userName, @RequestBody Map<String, Object> body) {
-    ResponseBody rb = new ResponseBody();
-
-    try {
-      String filterName = FieldValidator.validateFilterName(body, true);
-      Integer bucketId = null;
-      if (body.containsKey(COL.BUCKET_ID)) {
-        bucketId = (Integer) body.get(COL.BUCKET_ID);
-      }
-
-      List<Map<String, Object>> conditions = FieldValidator.validateConditions(body, true);
-      String description = FieldValidator.validateDescription(body, false);
-      Integer classId = FieldValidator.validateNullableId(body, COL.CLASS_ID, false);
-
-      int filterId = databucketService.createFilter(filterName, bucketId, userName, conditions, description, classId);
-      rb.setStatus(ResponseStatus.OK);
-      rb.setFilterId(filterId);
-      rb.setMessage("The new filter has been successfully created.");
-      return new ResponseEntity<>(rb, HttpStatus.CREATED);
-    } catch (ItemDoNotExistsException e) {
-      return customException(rb, e, HttpStatus.NOT_FOUND);
-    } catch (EmptyInputValueException | FilterAlreadyExistsException e1) {
-      return customException(rb, e1, HttpStatus.NOT_ACCEPTABLE);
-    } catch (Exception ee) {
-      return defaultException(rb, ee);
+    public FiltersController(DatabucketService databucketService) {
+        this.databucketService = databucketService;
     }
-  }
 
-  @DeleteMapping(value = "/filters/{filterId}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<ResponseBody> deleteFilters(@PathVariable Integer filterId, @RequestParam String userName) {
-    ResponseBody rb = new ResponseBody();
-    try {
-      databucketService.deleteFilter(userName, filterId);
-      rb.setStatus(ResponseStatus.OK);
-      rb.setMessage("The filter has been removed.");
-      return new ResponseEntity<>(rb, HttpStatus.OK);
-    } catch (Exception ee) {
-      return defaultException(rb, ee);
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseBody> createFilter(@RequestParam String userName, @RequestBody Map<String, Object> body) {
+        ResponseBody rb = new ResponseBody();
+
+        try {
+            String filterName = FieldValidator.validateFilterName(body, true);
+            Integer bucketId = null;
+            if (body.containsKey(COL.BUCKET_ID)) {
+                bucketId = (Integer) body.get(COL.BUCKET_ID);
+            }
+
+            List<Map<String, Object>> conditions = FieldValidator.validateConditions(body, true);
+            String description = FieldValidator.validateDescription(body, false);
+            Integer classId = FieldValidator.validateNullableId(body, COL.CLASS_ID, false);
+
+            int filterId = databucketService.createFilter(filterName, bucketId, userName, conditions, description, classId);
+            rb.setStatus(ResponseStatus.OK);
+            rb.setFilterId(filterId);
+            rb.setMessage("The new filter has been successfully created.");
+            return new ResponseEntity<>(rb, HttpStatus.CREATED);
+        } catch (ItemDoNotExistsException e) {
+            return customException(rb, e, HttpStatus.NOT_FOUND);
+        } catch (EmptyInputValueException | FilterAlreadyExistsException e1) {
+            return customException(rb, e1, HttpStatus.NOT_ACCEPTABLE);
+        } catch (Exception ee) {
+            return defaultException(rb, ee);
+        }
     }
-  }
 
-  @SuppressWarnings("unchecked")
-  @GetMapping(value = {
-      "",
-      "/{filterId}",
-      "/buckets/{bucketName}"}, produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<ResponseBody> getFilters(
-      @PathVariable Optional<String> bucketName,
-      @PathVariable Optional<Integer> filterId,
-      @RequestParam(required = false) Optional<Integer> page,
-      @RequestParam(required = false) Optional<Integer> limit,
-      @RequestParam(required = false) Optional<String> sort,
-      @RequestParam(required = false) Optional<String> filter) {
-
-    ResponseBody rb = new ResponseBody();
-    try {
-      if (page.isPresent()) {
-        FieldValidator.mustBeGraterThen0("page", page.get());
-        rb.setPage(page.get());
-      }
-
-      if (limit.isPresent()) {
-        FieldValidator.mustBeGraterOrEqual0("limit", limit.get());
-        rb.setLimit(limit.get());
-      }
-
-      if (sort.isPresent()) {
-        FieldValidator.validateSort(sort.get());
-        rb.setSort(sort.get());
-      }
-
-      List<Condition> urlConditions = null;
-      if (filter.isPresent()) {
-        urlConditions = FieldValidator.validateFilter(filter.get());
-      }
-
-      Map<String, Object> result = databucketService.getFilters(bucketName, filterId, page, limit, sort, urlConditions);
-
-      long total = (long) result.get(C.TOTAL);
-      rb.setTotal(total);
-
-      if (page.isPresent() && limit.isPresent()) {
-        rb.setTotalPages((int) Math.ceil(total / (float) limit.get()));
-      }
-
-      rb.setFilters((List<Map<String, Object>>) result.get(C.FILTERS));
-
-      return new ResponseEntity<>(rb, HttpStatus.OK);
-    } catch (ItemDoNotExistsException e) {
-      return customException(rb, e, HttpStatus.NOT_FOUND);
-    } catch (IncorrectValueException e2) {
-      return customException(rb, e2, HttpStatus.NOT_ACCEPTABLE);
-    } catch (Exception ee) {
-      return defaultException(rb, ee);
+    @DeleteMapping(value = "/{filterId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseBody> deleteFilters(@PathVariable Integer filterId, @RequestParam String userName) {
+        ResponseBody rb = new ResponseBody();
+        try {
+            databucketService.deleteFilter(userName, filterId);
+            rb.setStatus(ResponseStatus.OK);
+            rb.setMessage("The filter has been removed.");
+            return new ResponseEntity<>(rb, HttpStatus.OK);
+        } catch (ItemAlreadyUsedException e1) {
+            return customException(rb, e1, HttpStatus.NOT_ACCEPTABLE);
+        } catch (Exception ee) {
+            return defaultException(rb, ee);
+        }
     }
-  }
 
-  @PutMapping(value = "/{filterId}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<ResponseBody> modifyFilter(@PathVariable("filterId") Integer filterId, @RequestParam String userName, @RequestBody LinkedHashMap<String, Object> body) {
-    ResponseBody rb = new ResponseBody();
-    try {
-      Integer bucketId = FieldValidator.validateNullableId(body, COL.BUCKET_ID, false);
-      Integer classId = FieldValidator.validateNullableId(body, COL.CLASS_ID, false);
-      String filterName = FieldValidator.validateFilterName(body, false);
-      String description = FieldValidator.validateDescription(body, false);
-      List<Map<String, Object>> conditions = FieldValidator.validateConditions(body, false);
+    @SuppressWarnings("unchecked")
+    @GetMapping(value = {
+            "",
+            "/{filterId}",
+            "/buckets/{bucketName}"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseBody> getFilters(
+            @PathVariable Optional<String> bucketName,
+            @PathVariable Optional<Integer> filterId,
+            @RequestParam(required = false) Optional<Integer> page,
+            @RequestParam(required = false) Optional<Integer> limit,
+            @RequestParam(required = false) Optional<String> sort,
+            @RequestParam(required = false) Optional<String> filter) {
 
-      databucketService.modifyFilter(userName, filterId, filterName, bucketId, classId, description, conditions);
-      rb.setStatus(ResponseStatus.OK);
-      rb.setMessage("Filter with id '" + filterId + "' has been successfully modified.");
-      return new ResponseEntity<>(rb, HttpStatus.OK);
-    } catch (ItemDoNotExistsException e1) {
-      return customException(rb, e1, HttpStatus.NOT_FOUND);
-    } catch (EmptyInputValueException | ExceededMaximumNumberOfCharactersException e2) {
-      return customException(rb, e2, HttpStatus.NOT_ACCEPTABLE);
-    } catch (Exception ee) {
-      return defaultException(rb, ee);
+        ResponseBody rb = new ResponseBody();
+        try {
+            if (page.isPresent()) {
+                FieldValidator.mustBeGraterThen0("page", page.get());
+                rb.setPage(page.get());
+            }
+
+            if (limit.isPresent()) {
+                FieldValidator.mustBeGraterOrEqual0("limit", limit.get());
+                rb.setLimit(limit.get());
+            }
+
+            if (sort.isPresent()) {
+                FieldValidator.validateSort(sort.get());
+                rb.setSort(sort.get());
+            }
+
+            List<Condition> urlConditions = null;
+            if (filter.isPresent()) {
+                urlConditions = FieldValidator.validateFilter(filter.get());
+            }
+
+            Map<String, Object> result = databucketService.getFilters(bucketName, filterId, page, limit, sort, urlConditions);
+
+            long total = (long) result.get(C.TOTAL);
+            rb.setTotal(total);
+
+            if (page.isPresent() && limit.isPresent()) {
+                rb.setTotalPages((int) Math.ceil(total / (float) limit.get()));
+            }
+
+            rb.setFilters((List<Map<String, Object>>) result.get(C.FILTERS));
+
+            return new ResponseEntity<>(rb, HttpStatus.OK);
+        } catch (ItemDoNotExistsException e) {
+            return customException(rb, e, HttpStatus.NOT_FOUND);
+        } catch (IncorrectValueException e2) {
+            return customException(rb, e2, HttpStatus.NOT_ACCEPTABLE);
+        } catch (Exception ee) {
+            return defaultException(rb, ee);
+        }
     }
-  }
 
-  private ResponseEntity<ResponseBody> defaultException(ResponseBody rb, Exception e) {
-    logger.error("ERROR:", e);
-    rb.setStatus(ResponseStatus.FAILED);
-    rb.setMessage(e.getMessage());
-    return new ResponseEntity<>(rb, HttpStatus.INTERNAL_SERVER_ERROR);
-  }
+    @PutMapping(value = "/{filterId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseBody> modifyFilter(@PathVariable("filterId") Integer filterId, @RequestParam String userName, @RequestBody LinkedHashMap<String, Object> body) {
+        ResponseBody rb = new ResponseBody();
+        try {
+            Integer bucketId = FieldValidator.validateNullableId(body, COL.BUCKET_ID, false);
+            Integer classId = FieldValidator.validateNullableId(body, COL.CLASS_ID, false);
+            String filterName = FieldValidator.validateFilterName(body, false);
+            String description = FieldValidator.validateDescription(body, false);
+            List<Map<String, Object>> conditions = FieldValidator.validateConditions(body, false);
 
-  private ResponseEntity<ResponseBody> customException(ResponseBody rb, Exception e, HttpStatus status) {
-    logger.warn(e.getMessage());
-    rb.setStatus(ResponseStatus.FAILED);
-    rb.setMessage(e.getMessage());
-    return new ResponseEntity<>(rb, status);
-  }
+            databucketService.modifyFilter(userName, filterId, filterName, bucketId, classId, description, conditions);
+            rb.setStatus(ResponseStatus.OK);
+            rb.setMessage("Filter with id '" + filterId + "' has been successfully modified.");
+            return new ResponseEntity<>(rb, HttpStatus.OK);
+        } catch (ItemDoNotExistsException e1) {
+            return customException(rb, e1, HttpStatus.NOT_FOUND);
+        } catch (EmptyInputValueException | ExceededMaximumNumberOfCharactersException e2) {
+            return customException(rb, e2, HttpStatus.NOT_ACCEPTABLE);
+        } catch (Exception ee) {
+            return defaultException(rb, ee);
+        }
+    }
+
+    private ResponseEntity<ResponseBody> defaultException(ResponseBody rb, Exception e) {
+        logger.error("ERROR:", e);
+        rb.setStatus(ResponseStatus.FAILED);
+        rb.setMessage(e.getMessage());
+        return new ResponseEntity<>(rb, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<ResponseBody> customException(ResponseBody rb, Exception e, HttpStatus status) {
+        logger.warn(e.getMessage());
+        rb.setStatus(ResponseStatus.FAILED);
+        rb.setMessage(e.getMessage());
+        return new ResponseEntity<>(rb, status);
+    }
 }
--- src/main/java/pl/databucket/web/tags/TagsController.java
+++ src/main/java/pl/databucket/web/tags/TagsController.java
@@ -4,6 +4,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -19,11 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import pl.databucket.exception.EmptyInputValueException;
-import pl.databucket.exception.ExceededMaximumNumberOfCharactersException;
-import pl.databucket.exception.IncorrectValueException;
-import pl.databucket.exception.ItemDoNotExistsException;
-import pl.databucket.exception.TagAlreadyExistsException;
+import pl.databucket.exception.*;
 import pl.databucket.database.C;
 import pl.databucket.database.COL;
 import pl.databucket.database.Condition;
@@ -37,148 +34,150 @@ import pl.databucket.service.ResponseStatus;
 @RestController
 public class TagsController {
 
-  private static final Logger logger = LoggerFactory.getLogger(TagsController.class);
+    private static final Logger logger = LoggerFactory.getLogger(TagsController.class);
 
-  private final DatabucketService databucketService;
+    private final DatabucketService databucketService;
 
-  public TagsController(DatabucketService databucketService) {
-    this.databucketService = databucketService;
-  }
-
-  @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<ResponseBody> createTag(@RequestParam String userName, @RequestBody LinkedHashMap<String, Object> body) {
-
-    ResponseBody rb = new ResponseBody();
-
-    try {
-      Integer bucketId = FieldValidator.validateNullableId(body, COL.BUCKET_ID, false);
-      String bucketName = FieldValidator.validateBucketName(body, false);
-      String tagName = FieldValidator.validateTagName(body, true);
-      String description = FieldValidator.validateDescription(body, false);
-      String iconName = FieldValidator.validateIcon(body, false);
-      Integer classId = FieldValidator.validateNullableId(body, COL.CLASS_ID, false);
-
-      Integer tagId = databucketService.createTag(userName, tagName, bucketId, bucketName, iconName, description, classId);
-      rb.setStatus(ResponseStatus.OK);
-      rb.setTagId(tagId);
-      rb.setMessage("The new tag has been successfully created.");
-      return new ResponseEntity<>(rb, HttpStatus.CREATED);
-    } catch (ItemDoNotExistsException e1) {
-      return customException(rb, e1, HttpStatus.NOT_FOUND);
-    } catch (EmptyInputValueException | ExceededMaximumNumberOfCharactersException | IncorrectValueException | TagAlreadyExistsException e2) {
-      return customException(rb, e2, HttpStatus.NOT_ACCEPTABLE);
-    } catch (Exception ee) {
-      return defaultException(rb, ee);
+    public TagsController(DatabucketService databucketService) {
+        this.databucketService = databucketService;
     }
-  }
 
-  @DeleteMapping(value = "/{tagId}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<ResponseBody> deleteTags(@PathVariable Integer tagId, @RequestParam String userName) {
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseBody> createTag(@RequestParam String userName, @RequestBody LinkedHashMap<String, Object> body) {
 
-    ResponseBody rb = new ResponseBody();
+        ResponseBody rb = new ResponseBody();
 
-    try {
-      databucketService.deleteTag(userName, tagId);
-      rb.setStatus(ResponseStatus.OK);
-      rb.setMessage("The tag has been removed.");
-      return new ResponseEntity<>(rb, HttpStatus.OK);
-    } catch (Exception ee) {
-      return defaultException(rb, ee);
+        try {
+            Integer bucketId = FieldValidator.validateNullableId(body, COL.BUCKET_ID, false);
+            String bucketName = FieldValidator.validateBucketName(body, false);
+            String tagName = FieldValidator.validateTagName(body, true);
+            String description = FieldValidator.validateDescription(body, false);
+            String iconName = FieldValidator.validateIcon(body, false);
+            Integer classId = FieldValidator.validateNullableId(body, COL.CLASS_ID, false);
+
+            Integer tagId = databucketService.createTag(userName, tagName, bucketId, bucketName, iconName, description, classId);
+            rb.setStatus(ResponseStatus.OK);
+            rb.setTagId(tagId);
+            rb.setMessage("The new tag has been successfully created.");
+            return new ResponseEntity<>(rb, HttpStatus.CREATED);
+        } catch (ItemDoNotExistsException e1) {
+            return customException(rb, e1, HttpStatus.NOT_FOUND);
+        } catch (EmptyInputValueException | ExceededMaximumNumberOfCharactersException | IncorrectValueException | TagAlreadyExistsException e2) {
+            return customException(rb, e2, HttpStatus.NOT_ACCEPTABLE);
+        } catch (Exception ee) {
+            return defaultException(rb, ee);
+        }
     }
-  }
 
-  @SuppressWarnings("unchecked")
-  @GetMapping(value = {
-      "",
-      "/{tagId}",
-      "/buckets/{bucketName}"}, produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<ResponseBody> getTags(
-      @PathVariable Optional<String> bucketName,
-      @PathVariable Optional<Integer> tagId,
-      @RequestParam(required = false) Optional<Integer> page,
-      @RequestParam(required = false) Optional<Integer> limit,
-      @RequestParam(required = false) Optional<String> sort,
-      @RequestParam(required = false) Optional<String> filter) {
+    @DeleteMapping(value = "/{tagId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseBody> deleteTags(@PathVariable Integer tagId, @RequestParam String userName) {
 
-    ResponseBody rb = new ResponseBody();
+        ResponseBody rb = new ResponseBody();
 
-    try {
-      if (page.isPresent()) {
-        FieldValidator.mustBeGraterThen0("page", page.get());
-        rb.setPage(page.get());
-      }
-
-      if (limit.isPresent()) {
-        FieldValidator.mustBeGraterOrEqual0("limit", limit.get());
-        rb.setLimit(limit.get());
-      }
-
-      if (sort.isPresent()) {
-        FieldValidator.validateSort(sort.get());
-        rb.setSort(sort.get());
-      }
-
-      List<Condition> urlConditions = null;
-      if (filter.isPresent()) {
-        urlConditions = FieldValidator.validateFilter(filter.get());
-      }
-
-      Map<String, Object> result = databucketService.getTags(bucketName, tagId, page, limit, sort, urlConditions);
-
-      long total = (long) result.get(C.TOTAL);
-      rb.setTotal(total);
-
-      if (page.isPresent() && limit.isPresent()) {
-        rb.setTotalPages((int) Math.ceil(total / (float) limit.get()));
-      }
-
-      rb.setTags((List<Map<String, Object>>) result.get(C.TAGS));
-
-      return new ResponseEntity<>(rb, HttpStatus.OK);
-    } catch (ItemDoNotExistsException e1) {
-      return customException(rb, e1, HttpStatus.NOT_FOUND);
-    } catch (IncorrectValueException e2) {
-      return customException(rb, e2, HttpStatus.NOT_ACCEPTABLE);
-    } catch (Exception ee) {
-      return defaultException(rb, ee);
+        try {
+            databucketService.deleteTag(userName, tagId);
+            rb.setStatus(ResponseStatus.OK);
+            rb.setMessage("The tag has been removed.");
+            return new ResponseEntity<>(rb, HttpStatus.OK);
+        } catch (ItemAlreadyUsedException e1) {
+            return customException(rb, e1, HttpStatus.NOT_ACCEPTABLE);
+        } catch (Exception ee) {
+            return defaultException(rb, ee);
+        }
     }
-  }
 
-  @PutMapping(value = "/{tagId}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<ResponseBody> modifyTag(@PathVariable("tagId") Integer tagId, @RequestParam String userName, @RequestBody LinkedHashMap<String, Object> body) {
+    @SuppressWarnings("unchecked")
+    @GetMapping(value = {
+            "",
+            "/{tagId}",
+            "/buckets/{bucketName}"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseBody> getTags(
+            @PathVariable Optional<String> bucketName,
+            @PathVariable Optional<Integer> tagId,
+            @RequestParam(required = false) Optional<Integer> page,
+            @RequestParam(required = false) Optional<Integer> limit,
+            @RequestParam(required = false) Optional<String> sort,
+            @RequestParam(required = false) Optional<String> filter) {
 
-    ResponseBody rb = new ResponseBody();
+        ResponseBody rb = new ResponseBody();
 
-    try {
-      Integer bucketId = FieldValidator.validateNullableId(body, COL.BUCKET_ID, false);
-      Integer classId = FieldValidator.validateNullableId(body, COL.CLASS_ID, false);
-      String tagName = FieldValidator.validateTagName(body, false);
-      String description = FieldValidator.validateDescription(body, false);
+        try {
+            if (page.isPresent()) {
+                FieldValidator.mustBeGraterThen0("page", page.get());
+                rb.setPage(page.get());
+            }
 
-      databucketService.modifyTag(userName, tagId, tagName, bucketId, classId, description);
-      rb.setStatus(ResponseStatus.OK);
-      rb.setMessage("Tag with id '" + tagId + "' has been successfully modified.");
-      return new ResponseEntity<>(rb, HttpStatus.OK);
-    } catch (ItemDoNotExistsException e1) {
-      return customException(rb, e1, HttpStatus.NOT_FOUND);
-    } catch (TagAlreadyExistsException | IncorrectValueException | ExceededMaximumNumberOfCharactersException e2) {
-      return customException(rb, e2, HttpStatus.NOT_ACCEPTABLE);
-    } catch (Exception ee) {
-      return defaultException(rb, ee);
+            if (limit.isPresent()) {
+                FieldValidator.mustBeGraterOrEqual0("limit", limit.get());
+                rb.setLimit(limit.get());
+            }
+
+            if (sort.isPresent()) {
+                FieldValidator.validateSort(sort.get());
+                rb.setSort(sort.get());
+            }
+
+            List<Condition> urlConditions = null;
+            if (filter.isPresent()) {
+                urlConditions = FieldValidator.validateFilter(filter.get());
+            }
+
+            Map<String, Object> result = databucketService.getTags(bucketName, tagId, page, limit, sort, urlConditions);
+
+            long total = (long) result.get(C.TOTAL);
+            rb.setTotal(total);
+
+            if (page.isPresent() && limit.isPresent()) {
+                rb.setTotalPages((int) Math.ceil(total / (float) limit.get()));
+            }
+
+            rb.setTags((List<Map<String, Object>>) result.get(C.TAGS));
+
+            return new ResponseEntity<>(rb, HttpStatus.OK);
+        } catch (ItemDoNotExistsException e1) {
+            return customException(rb, e1, HttpStatus.NOT_FOUND);
+        } catch (IncorrectValueException e2) {
+            return customException(rb, e2, HttpStatus.NOT_ACCEPTABLE);
+        } catch (Exception ee) {
+            return defaultException(rb, ee);
+        }
     }
-  }
 
-  private ResponseEntity<ResponseBody> defaultException(ResponseBody rb, Exception e) {
-    logger.error("ERROR:", e);
-    rb.setStatus(ResponseStatus.FAILED);
-    rb.setMessage(e.getMessage());
-    return new ResponseEntity<>(rb, HttpStatus.INTERNAL_SERVER_ERROR);
-  }
+    @PutMapping(value = "/{tagId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseBody> modifyTag(@PathVariable("tagId") Integer tagId, @RequestParam String userName, @RequestBody LinkedHashMap<String, Object> body) {
 
-  private ResponseEntity<ResponseBody> customException(ResponseBody rb, Exception e, HttpStatus status) {
-    logger.warn(e.getMessage());
-    rb.setStatus(ResponseStatus.FAILED);
-    rb.setMessage(e.getMessage());
-    return new ResponseEntity<>(rb, status);
-  }
+        ResponseBody rb = new ResponseBody();
+
+        try {
+            Integer bucketId = FieldValidator.validateNullableId(body, COL.BUCKET_ID, false);
+            Integer classId = FieldValidator.validateNullableId(body, COL.CLASS_ID, false);
+            String tagName = FieldValidator.validateTagName(body, false);
+            String description = FieldValidator.validateDescription(body, false);
+
+            databucketService.modifyTag(userName, tagId, tagName, bucketId, classId, description);
+            rb.setStatus(ResponseStatus.OK);
+            rb.setMessage("Tag with id '" + tagId + "' has been successfully modified.");
+            return new ResponseEntity<>(rb, HttpStatus.OK);
+        } catch (ItemDoNotExistsException e1) {
+            return customException(rb, e1, HttpStatus.NOT_FOUND);
+        } catch (TagAlreadyExistsException | IncorrectValueException | ExceededMaximumNumberOfCharactersException e2) {
+            return customException(rb, e2, HttpStatus.NOT_ACCEPTABLE);
+        } catch (Exception ee) {
+            return defaultException(rb, ee);
+        }
+    }
+
+    private ResponseEntity<ResponseBody> defaultException(ResponseBody rb, Exception e) {
+        logger.error("ERROR:", e);
+        rb.setStatus(ResponseStatus.FAILED);
+        rb.setMessage(e.getMessage());
+        return new ResponseEntity<>(rb, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<ResponseBody> customException(ResponseBody rb, Exception e, HttpStatus status) {
+        logger.warn(e.getMessage());
+        rb.setStatus(ResponseStatus.FAILED);
+        rb.setMessage(e.getMessage());
+        return new ResponseEntity<>(rb, status);
+    }
 }
