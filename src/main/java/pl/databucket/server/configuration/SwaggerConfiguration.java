6b2226fb346fb46fca96c18f2022f67f21a25bcf
Krzysiek_QM <krzysztof.slysz@qualityminds.pl>
2023-02-21 15:42:17 +0100

    v.3.5.0 - migrated to java 17 - upgraded spring to 2.5.7

--- src/main/java/pl/databucket/server/configuration/SwaggerConfiguration.java
+++ src/main/java/pl/databucket/server/configuration/SwaggerConfiguration.java
@@ -48,7 +48,7 @@ public class SwaggerConfiguration {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Databucket API")
-                .version("3.4.4")
+                .version("3.5.0")
                 .build();
     }
 }
