6468c5ff6e8dea1544c6707582107adbac892e60
Ariel Guelfi <arguelfi@gmail.com>
2023-03-01 14:47:48 +0100

    Bring back maxheight on datatable to apply sticky headers; give a bit more space to mini drawer

--- frontend/src/components/data/_ProjectData.jsx
+++ frontend/src/components/data/_ProjectData.jsx
@@ -58,9 +58,9 @@ const closedMixin = (theme) => ({
         duration: theme.transitions.duration.leavingScreen,
     }),
     overflowX: 'hidden',
-    width: `calc(${theme.spacing(7)} + 1px)`,
+    width: `calc(${theme.spacing(8)} + 1px)`,
     [theme.breakpoints.up('sm')]: {
-        width: `calc(${theme.spacing(8)} + 1px)`,
+        width: `calc(${theme.spacing(9)} + 1px)`,
     },
 });
 
