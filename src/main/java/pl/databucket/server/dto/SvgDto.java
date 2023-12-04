574f947a2b32c3fe15a819b0c126bfab1f6aa82d
Ariel Guelfi <ariel.guelfi@orange.com>
2023-12-04 10:52:31 +0100

    Fix missing default constructor for jackson

--- src/main/java/pl/databucket/server/dto/SvgDto.java
+++ src/main/java/pl/databucket/server/dto/SvgDto.java
@@ -2,13 +2,17 @@ package pl.databucket.server.dto;
 
 import java.util.Date;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @Data
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
 public class SvgDto {
 
     Long id;
